fcee4f0b5192d0c8990bbcb339ec91793597c5b1
Ruyu You <727809166@qq.com>
2016-07-31 20:34:47 +0800

    add app title

---
+++ views/app.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Todos</h1>
+            </div>
+        );
+    }
+}
+
+/*
+import React from 'react';
+
+export default class TodoBox extends React.Component {
+    render() {
+        return (
+            <div className="todoBox">
+                <h1>Todos</h1>
+                <TodoList />
+                <TodoForm />
+            </div>
+        );
+    }
+}
+
+class TodoList extends React.Component {
+    render() {
+        return (
+            <div className="todoList">
+                <table style={{border: "1px solid black"}}>
+                    <tbody>
+                    <Todo title="Shopping">Milk</Todo>
+                    <Todo title="Hair cut">13:00</Todo>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+class Todo extends React.Component {
+    render() {
+        return (
+            <tr>
+                <td style={{border: "1px solid black"}}>{this.props.title}</td>
+                <td style={{border: "1px solid black"}}>{this.props.children}</td>
+            </tr>
+        );
+    }
+}
+
+class TodoForm extends React.Component {
+    render() {
+        return (
+            <div className="todoForm">
+                I am a TodoForm.
+            </div>
+        );
+    }
+}*/
