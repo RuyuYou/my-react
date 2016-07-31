7c111a5a381183ad2408ea08f341725396f53674
Ruyu You <727809166@qq.com>
2016-07-31 19:55:59 +0800

    add server.js

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
-/**
- * Created by you on 16-7-31.
- */
+const express = require('express');
+const app = express();
+
+const expressReactViews = require('express-react-views');
+// const Babel = require('babel/register');
+
+app.set('view engine', 'jsx');
+app.set('views', __dirname + '/views');
+
+app.engine('jsx', expressReactViews.createEngine({transformViews: false}));
+
+// Babel({ignore: false});
+require('babel/register')({
+    ignore: false
+});
+app.use('/', function (req, res) {
+    res.render('index', '');
+});
+app.listen(3000, function () {
+    console.log('Listen on 3000,Server is running..');
+});
